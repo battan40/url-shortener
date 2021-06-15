2c12db0af25542e31fc371accc0bf9bb3f9315a9
Angela Battillo <angela.battillo@gmail.com>
2021-06-15 10:44:37 -0600

    Make function for post call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,22 @@
 export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
-    
+
+}
+
+export const postUrls = (newUrl) => {
+  const init = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newUrl),
+  }
+  return fetch('http://localhost:3001/api/v1/urls', init)
+         .then(response => {
+           if(response.ok) {
+             return response.json()
+           }
+         })
+        .catch(err => 'Looks like the trouble is on us.  Please try back later.')
 }
